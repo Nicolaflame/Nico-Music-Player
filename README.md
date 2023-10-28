d0455f18914605438c17befb6f464a7115766d7a
Nicolaflame <144032765+Nicolaflame@users.noreply.github.com>
2023-10-28 12:00:05 -0400

    Working player (with error)
    Player is working but I have an error:
    "Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')
    at app.js:41:29"

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Nico-Music-Player
+
+This is a project created by following along with Modern Web's youtube tutorial, 'How to create Music player with pure HTML, CSS, JS.'
+
+link: https://www.youtube.com/watch?v=Zf_psaJ-wP4
+
+still need to complete the tutorial, but ran out of time today. Ended at: 29:41 in video.
+
+Player is working but I have an error:
+"Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')
+at app.js:41:29"
--- data.js
+++ data.js
@@ -1,8 +1,39 @@
+"use strict";
 let songs = [
   {
     name: "Apart of Me",
     path: "Music/Apart Of Me- OTR & DEVAULT .mp3",
-    Artist: "OTR & Devault",
+    artist: "OTR & Devault",
     cover: "Images/Apart-of-me.jpeg",
   },
+  // {
+  //   name: "Song 1",
+  //   path: "Music/Song 1.mp3",
+  //   artist: "Artist 1",
+  //   cover: "Images/cover 1.png",
+  // },
+  // {
+  //   name: "Song 2",
+  //   path: "Music/Song 2.mp3",
+  //   artist: "Artist 2",
+  //   cover: "Images/cover 2.png",
+  // },
+  // {
+  //   name: "Song 3",
+  //   path: "Music/Song 3.mp3",
+  //   artist: "Artist 3",
+  //   cover: "Images/cover 3.png",
+  // },
+  // {
+  //   name: "Song 4",
+  //   path: "Music/Song 4.mp3",
+  //   artist: "Artist 4",
+  //   cover: "Images/cover 4.png",
+  // },
+  // {
+  //   name: "Song 5",
+  //   path: "Music/Song 5.mp3",
+  //   artist: "Artist 5",
+  //   cover: "Images/cover 5.png",
+  // },
 ];
